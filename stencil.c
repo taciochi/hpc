8c6582b6ff984df9f72a4fc06d6c9edf47db653a
taciochi <ciochirca.teodoravram98@gmail.com>
2023-05-01 19:33:02 +0300

    s

--- stencil.c
+++ stencil.c
@@ -43,6 +43,8 @@
 // }
 
 void stencil(float* inputvec, int m, int n, float* filtervec, int k, float* outputvec, int b) {
+    int i, j, l, x, y;
+
     int km = (k+1)/2;
     int blower = (k-1)/2;
     int bupper = (k-1)/2;
@@ -52,9 +54,9 @@ void stencil(float* inputvec, int m, int n, float* filtervec, int k, float* outp
     float (*output)[m][n] = (float (*)[m][n]) outputvec;
 
     #pragma omp parallel for schedule(dynamic) collapse(2)
-    for (int i=0; i<b; i++) {
-        for (int j=0; j<m; j++) {
-            for (int l=0; l<n; l++) {
+    for (i=0; i<b; i++) {
+        for (j=0; j<m; j++) {
+            for (l=0; l<n; l++) {
                 if (j<km-1+blower || j>=(m-km+1+bupper) || l<km-1+blower || l>=(n-km+1+bupper)) {
                     output[i][j][l] = input[i][j][l];
                 } else {
