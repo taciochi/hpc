9d86b99ee9bc948c7a1f33c3753acaec3e9269f4
taciochi <ciochirca.teodoravram98@gmail.com>
2023-05-10 02:20:31 +0300

    c

---
+++ slurm-stencil.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -l
+
+#SBATCH -p course -t 2
+#SBATCH -D ./
+#SBATCH --export=ALL
+
+# load modules intel compiler
+module load compilers/intel/2019u5
+module load mpi/intel-mpi/2019u5/bin
+
+make gccserial
+make gcccomplete
+make iccserial
+make icccomplete
+
+threads=(1 2 4 8 16 32)
+
+for i in "${threads[@]}"
+do
+    echo "Runnin gcc OpenMP stencil with $i threads:"
+    export OMP_NUM_THREADS=$i
+    ./stencil-omp-gcc.exe
+    echo "---"
+
+done
+
+for i in "${threads[@]}"
+do
+    echo "Runnin icc OpenMP stencil with $i threads:"
+    export OMP_NUM_THREADS=$i
+    ./stencil-omp-icc.exe
+    echo "---"
+
+done
+
+for i in "${threads[@]}"
+do
+    echo "Runnin gcc MPI stencil with $i ranks:"
+    mpiexec -n $i ./stencil-complete-gcc.exe
+    echo "---"
+
+done
+
+for i in "${threads[@]}"
+do
+    echo "Runnin icc MPI stencil with $i ranks:"
+    mpiexec -n $i ./stencil-complete-icc.exe
+    echo "---"
+
+done
--- stencil.c
+++ stencil.c
@@ -1,7 +1,7 @@
 #include <omp.h>
 
 void stencil(float *inputvec, int m, int n, float *filtervec, int k, float *outputvec, int b) {
-    int batch, i, j, x, y;
+    int batch, row, col, x, y;
 
     int km = (k + 1) / 2;
     int blower = (k - 1) / 2;
@@ -11,20 +11,20 @@ void stencil(float *inputvec, int m, int n, float *filtervec, int k, float *outp
     float (*filter)[k] = (float(*)[k])filtervec;
     float (*output)[m][n] = (float(*)[m][n])outputvec;
 
-    #pragma omp parallel for private(batch, i, j, x, y) shared(output)
+    #pragma omp parallel for private(batch, row, col, x, y) shared(output)
     for (batch = 0; batch < b; batch++) {
-        for (i = blower; i < m - bupper; i++) {
-            for (j = blower; j < n - bupper; j++) {
+        for (row = blower; row < m - bupper; row++) {
+            for (col = blower; col < n - bupper; col++) {
 
                 for (x = 0; x < k; x++) {
                     for (y = 0; y < k; y++) {
-                        int input_i = i - km + x;
-                        int input_j = j - km + y;
+                        int input_row = row - km + x;
+                        int input_col = col - km + y;
                         int filter_x = x;
                         int filter_y = y;
 
-                        if (input_i >= 0 && input_i < m && input_j >= 0 && input_j < n) {
-                            output[batch][i][j] += input[batch][input_i][input_j] * filter[filter_x][filter_y];
+                        if (input_row >= 0 && input_row < m && input_col >= 0 && input_col < n) {
+                            output[batch][row][col] += input[batch][input_row][input_col] * filter[filter_x][filter_y];
                         }
                     }
                 }
