2e2db6852acd9ad4044cc4ccb397d8bd9092dd79
taciochi <ciochirca.teodoravram98@gmail.com>
2023-05-10 00:37:37 +0300

    c

--- stencil.c
+++ stencil.c
@@ -2,7 +2,6 @@
 
 void stencil(float *inputvec, int m, int n, float *filtervec, int k, float *outputvec, int b) {
     int batch, i, j, x, y;
-
     int km = (k + 1) / 2;
     int blower = (k - 1) / 2;
     int bupper = (k - 1) / 2;
@@ -11,16 +10,20 @@ void stencil(float *inputvec, int m, int n, float *filtervec, int k, float *outp
     float (*filter)[k] = (float(*)[k])filtervec;
     float (*output)[m][n] = (float(*)[m][n])outputvec;
 
-    #pragma omp parallel for private(batch, i, j, x, y) shared(input, filter, output)
+    #pragma omp parallel for private(batch, i, j, x, y)
     for (batch = 0; batch < b; batch++) {
-        #pragma omp parallel for private(i, j, x, y) shared(input, filter, output)
         for (i = blower; i < m - bupper; i++) {
-            #pragma omp parallel for private(j, x, y) shared(input, filter, output)
             for (j = blower; j < n - bupper; j++) {
                 output[batch][i][j] = 0.0;
+
                 for (x = 0; x < k; x++) {
                     for (y = 0; y < k; y++) {
-                        output[batch][i][j] += input[batch][i - blower + x][j - blower + y] * filter[x][y];
+                        int input_i = i - blower + x;
+                        int input_j = j - blower + y;
+                        int filter_x = x;
+                        int filter_y = y;
+
+                        output[batch][i][j] += input[batch][input_i][input_j] * filter[filter_x][filter_y];
                     }
                 }
             }
