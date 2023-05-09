21707569c66d1422bbbe58160f0139ff7f1ca70c
taciochi <ciochirca.teodoravram98@gmail.com>
2023-05-10 02:34:47 +0300

    c

--- stencil.c
+++ stencil.c
@@ -11,9 +11,11 @@ void stencil(float *inputvec, int m, int n, float *filtervec, int k, float *outp
     float (*filter)[k] = (float(*)[k])filtervec;
     float (*output)[m][n] = (float(*)[m][n])outputvec;
 
-    #pragma omp parallel private(batch, row, col, x, y)
+    float private_output[b][m][n];  // Private temporary array for each thread
+
+    #pragma omp parallel private(batch, row, col, x, y, private_output)
     {
-        float private_output[b][m][n];
+        // Initialize the private temporary array to zeros
         memset(private_output, 0, sizeof(private_output));
 
         #pragma omp for
@@ -36,13 +38,13 @@ void stencil(float *inputvec, int m, int n, float *filtervec, int k, float *outp
             }
         }
 
-        #pragma omp critical
-        {
-            for (batch = 0; batch < b; batch++) {
-                for (row = blower; row < m - bupper; row++) {
-                    for (col = blower; col < n - bupper; col++) {
-                        output[batch][row][col] += private_output[batch][row][col];
-                    }
+        // Combine the results into the output array using atomic updates
+        #pragma omp for
+        for (batch = 0; batch < b; batch++) {
+            for (row = blower; row < m - bupper; row++) {
+                for (col = blower; col < n - bupper; col++) {
+                    #pragma omp atomic
+                    output[batch][row][col] += private_output[batch][row][col];
                 }
             }
         }
