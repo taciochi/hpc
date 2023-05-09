a5f761afbba16f754a9e5cbc6fdd04f51ef9e003
taciochi <ciochirca.teodoravram98@gmail.com>
2023-05-09 23:03:17 +0300

    c

--- main-mpi.c
+++ main-mpi.c
@@ -11,6 +11,19 @@ void write_to_output_file(char *filename, float *output, int *dims, int num_dims
 long int product(int *array, int n);
 void stencil(float* inputvec, int m, int n, float* filtervec, int k, float* output, int b);
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<stdbool.h>
+#include<mpi.h>
+
+int *read_dims(char *filename);
+float *read_array(char *filename, int *dims, int num_dims);
+void write_to_output_file(char *filename, float *output, int *dims, int num_dims);
+long int product(int *array, int n);
+void stencil(float* inputvec, int m, int n, float* filtervec, int k, float* output, int b);
+
 int main(int argc, char *argv[]){
     int rank, size;
     MPI_Init(&argc, &argv);
@@ -202,4 +215,150 @@ void stencil(float* inputvec, int m, int n, float* filtervec, int k, float* outp
     }
 
     MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, output, input_size/size, MPI_FLOAT, MPI_COMM_WORLD);
+}
+/*Code for reading and writing to the files*/
+
+/*Gets the dimensions of the matrices. This will return a 1d array of 4 elements [0] = number of dimensions [1] = batch [2] = m [3] = n*/
+int *read_dims(char *filename) {
+    FILE *file = fopen(filename,"r");
+    int i;
+    
+    if(file == NULL) {
+        printf("Unable to open file: %s", filename);
+        return NULL;
+    }
+
+    char firstline[500];
+    fgets(firstline, 500, file);
+    
+    int line_length = strlen(firstline);
+
+    int num_dims = 0;
+    
+    for(i=0; i<line_length; i++) {  
+        if(firstline[i] == ' ') {
+            num_dims++;
+        }
+    }
+    
+    int *dims = malloc((num_dims+1)*sizeof(int));
+    dims[0] = num_dims;
+    const char s[2] = " ";
+    char *token;
+    token = strtok(firstline, s);
+    i = 0;
+    while( token != NULL ) {
+        dims[i+1] = atoi(token);
+        i++;
+        token = strtok(NULL, s);
+    }
+    fclose(file);
+    return dims;
+}
+
+/*Gets the data from the file and returns it as a 1 dimensional array*/
+float * read_array(char *filename, int *dims, int num_dims) {
+    FILE *file = fopen(filename,"r");
+    int i;
+    
+    if(file == NULL) {
+        printf("Unable to open file: %s", filename);
+        return NULL;
+    }
+
+    char firstline[500];
+    fgets(firstline, 500, file);
+
+    //Ignore first line and move on since first line contains 
+    //header information and we already have that. 
+
+    long int total_elements = product(dims, num_dims);
+
+    float *one_d = malloc(sizeof(float) * total_elements);
+    
+    for(i=0; i<total_elements; i++) {
+        fscanf(file, "%f", &one_d[i]);
+    }
+    fclose(file);
+    return one_d;
+}
+
+/*Writes to the output file*/
+void write_to_output_file(char *filename, float *output, int *dims, int num_dims){
+    FILE *file = fopen(filename,"w");
+    int i;
+    
+    if(file == NULL) {
+        printf("Unable to open file: %s", filename);
+        return;
+    }
+
+    if (file != NULL) {
+        for(i=0; i<num_dims; i++) {
+            fprintf(file, "%d ", dims[i]);
+        }
+        fprintf(file, "\n");
+    }
+      
+    long int total_elements = product(dims, num_dims);
+      
+    for(i=0; i<total_elements; i++) {
+        fprintf(file, "%.7f ", output[i]);
+    }
+
+    fclose(file);
+}
+
+/*Returns the number of elements by multiplying the dimensions*/
+long int product(int *array, int n) {
+    long int product = 1;
+    int i;
+    for(i=0; i<n; i++) {
+        product *= array[i];
+    }
+    return product;
+}
+
+/*Performs the stencil operation on the input array using the filter array*/
+void stencil(float* inputvec, int m, int n, float* filtervec, int k, float* output, int b) {
+    int i, j, l, p, q;
+    int h = k / 2;
+    int input_size = m * n * sizeof(float);
+
+    float* input = inputvec;
+    float* filter = filtervec;
+    float* output = outputvec;
+
+    int rank, size;
+    if (MPI_Comm_rank(MPI_COMM_WORLD, &rank) != MPI_SUCCESS) {
+        fprintf(stderr, "Error: MPI_Comm_rank failed.\n");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+    if (MPI_Comm_size(MPI_COMM_WORLD, &size) != MPI_SUCCESS) {
+        fprintf(stderr, "Error: MPI_Comm_size failed.\n");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+
+    for (p=0; p<b; p++) {
+        for (i=rank; i<m; i+=size) {
+            for (j=0; j<n; j++) {
+                float sum = 0.0f;
+                for (l=-h; l<=h; l++) {
+                    for (q=-h; q<=h; q++) {
+                        int ii = i + l;
+                        int jj = j + q;
+                        if (ii >= 0 && ii < m && jj >= 0 && jj < n) {
+                            sum += input[p*m*n + ii*n + jj] * filter[(l+h)*k + (q+h)];
+                        }
+                    }
+                }
+                output[p*m*n + i*n + j] = sum;
+            }
+        }
+    }
+
+    if (MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, output, input_size/size, MPI_FLOAT, MPI_COMM_WORLD) != MPI_SUCCESS) {
+        fprintf(stderr, "Error: MPI_Allgather failed.\n");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
 }
